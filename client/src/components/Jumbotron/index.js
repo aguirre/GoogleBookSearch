05a80c2138d8cd584b5f3a199e2f392b1130e215
thirteen <41817587+maguirre13@users.noreply.github.com>
2019-01-26 00:30:14 -0800

    add jumbotron component

---
+++ client/src/components/Jumbotron/index.js
@@ -0,0 +1,24 @@
+import React from "react";
+
+const Jumbotron = ({ children }) => {
+  return (
+    <div
+      style={{ textAlign: "center", backgroundColor: "skyblue" }}
+      className="jumbotron"
+    >
+      <img
+        src="./react-logo.png"
+        alt="react-logo"
+        style={{ height: "200px" }}
+      />
+      <img
+        src="./google-logo.png"
+        alt="google-logo"
+        style={{ height: "200px" }}
+      />
+      <img src="./book-icon.png" alt="book-icon" style={{ height: "200px" }} />
+    </div>
+  );
+};
+
+export default Jumbotron;
